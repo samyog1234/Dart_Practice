1217847cc65e2e7717fe5e99077353089358830e
samyog1234 <126430228+samyog1234@users.noreply.github.com>
2023-09-11 16:09:39 +0545

    Recursive and Lambda Functions

---
+++ function.dart
@@ -0,0 +1,22 @@
+// // Recursive Function:
+// factorial(number) {
+//   if (number <= 0) {
+//     return 1;
+//   } else {
+//     return number * factorial(number - 1);
+//   }
+// }
+
+// void main() {
+//   print(factorial(6));
+// }
+
+//Limbda Function:
+
+printMsg() => print("Hello");
+int test() => 123;
+
+void main() {
+  printMsg();
+  print(test());
+}
