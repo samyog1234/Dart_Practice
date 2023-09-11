48963004d02af5abb03d808fe858c11c16899da8
samyog1234 <126430228+samyog1234@users.noreply.github.com>
2023-09-11 15:56:14 +0545

    practiced Runes

---
+++ Runes.dart
@@ -0,0 +1,40 @@
+import 'dart:core';
+
+// things we are using in Runes:
+// 1. String.codeUnitsAt();
+// 2. String.codeUnits;
+// 3. String.runes;
+
+// void main() {
+//   f1();
+
+// }
+
+// f1() {
+//   String x = "Runes";
+//   print(x.codeUnitAt(0));
+// }
+
+// void main() {
+//   f2();
+// }
+
+// f2() {
+//   String x = "Runes";
+//   print(x.codeUnits);
+// }
+
+// void main() {
+//   "A string".runes.forEach((int rune) {
+//     var character = new String.fromCharCode(rune);
+//     print(character);
+//   });
+// }
+
+// TO show emoji
+main() {
+  Runes input = new Runes(' \u{1f611} ');
+  Runes output = new Runes("\u{1f610}");
+  print(new String.fromCharCodes(input));
+  print(new String.fromCharCodes(output));
+}
