2e224d8c81ebe52fcd5f8789ed365561159affb0
samyog1234 <126430228+samyog1234@users.noreply.github.com>
2023-09-11 15:56:46 +0545

    Practiced Enumeration in dart programming

---
+++ Enumeration.dart
@@ -0,0 +1,41 @@
+// enum Status {
+//   none,
+//   running,
+//   stopped,
+//   paused
+// }
+
+// void main() {
+//   print(Status.values);
+//   Status.values.forEach((v) => print('value: $v, index: ${v.index}'));
+//   print("running: ${Status.running}, ${Status.running.index} ");
+//   print("running index: ${Status.values[1]}");
+// }
+
+enum DaysOfWeek {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+}
+
+void main() {
+  var today = DaysOfWeek.Tuesday;
+
+  switch (today) {
+    case DaysOfWeek.Monday:
+      print("It's MOnday. The start of the workweek");
+      break;
+    case DaysOfWeek.Wednesday:
+      print("It's Wednesday. Haldway through the routing");
+      break;
+    case DaysOfWeek.Friday:
+      print("It's Friday. The weekend is almost here!");
+      break;
+    default:
+      print("It's not a special day");
+  }
+}
+
