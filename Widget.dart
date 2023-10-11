395070aed3284d54fd18df04f88073adee410630
Samyog Koirala <126430228+samyog1234@users.noreply.github.com>
2023-10-11 18:02:20 +0545

    Update Widget.dart

--- Widget.dart
+++ Widget.dart
@@ -1,24 +1,24 @@
 import "package:flutter/material.dart";
 
-// void main() {
-//   runApp(MyApp());
-// }
+void main() {
+  runApp(MyApp());
+}
 
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: const Text('My Flutter App'),
-//         ),
-//         body: const Center(
-//           child: Text('Hello, Flutter!'),
-//         ),
-//       ),
-//     );
-//   }
-// }
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My Flutter App'),
+        ),
+        body: const Center(
+          child: Text('Hello, Flutter!'),
+        ),
+      ),
+    );
+  }
+}
 
 void main() {
   runApp(Hello());
@@ -47,61 +47,61 @@ class Hello extends StatelessWidget {
   }
 }
 
-// import "package:flutter/material.dart";
+import "package:flutter/material.dart";
 
-// void main() {
-//   runApp(Fyou());
-// }
+void main() {
+  runApp(Fyou());
+}
 
-// class Fyou extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp (
-//     home: Scaffold(
-//       appBar: AppBar(
-//       title: const Text("Hello From God",
-//                        style: TextStyle(
-//                        color: Colors.black)
-//                        ),
-//       backgroundColor: Colors.red
-//       ),
-//       body: Container (
-//       color: Colors.black,
-//         child: const Center (
-//         child: Text("This is Hello form God.",
-//                  style: TextStyle(
-//                  color: Colors.white)
-//                  )
-//       )
-//       )
-//     )
-//     );
-//   }
-// }
+class Fyou extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp (
+    home: Scaffold(
+      appBar: AppBar(
+      title: const Text("Hello From God",
+                       style: TextStyle(
+                       color: Colors.black)
+                       ),
+      backgroundColor: Colors.red
+      ),
+      body: Container (
+      color: Colors.black,
+        child: const Center (
+        child: Text("This is Hello form God.",
+                 style: TextStyle(
+                 color: Colors.white)
+                 )
+      )
+      )
+    )
+    );
+  }
+}
 
 
-// class Fyou extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp (
-//     home: Scaffold (
-//       appBar: AppBar(
-//       title: const Text("Hello form the Devil",
-//                        style: TextStyle(
-//                        color: Colors.black)
-//                        ),
-//       backgroundColor: Colors.red
-//       ),
-//       body: Container (
-//       color: Colors.black,
-//         child: const Center(
-//         child: Text("This is hello form the Devil",
-//                    style: TextStyle(
-//                    color: Colors.white)
-//                    )
-//         )
-//       )
-//     )
-//     );
-//   }
-// }
+class Fyou extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp (
+    home: Scaffold (
+      appBar: AppBar(
+      title: const Text("Hello form the Devil",
+                       style: TextStyle(
+                       color: Colors.black)
+                       ),
+      backgroundColor: Colors.red
+      ),
+      body: Container (
+      color: Colors.black,
+        child: const Center(
+        child: Text("This is hello form the Devil",
+                   style: TextStyle(
+                   color: Colors.white)
+                   )
+        )
+      )
+    )
+    );
+  }
+}
